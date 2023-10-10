bc2b79669242522514a87c0141c6b2848e5b2bdd
TranD0 <trthaingocdo@gmail.com>
2023-10-10 12:43:21 +0700

    lam btvn
    trang 31 32

--- Exercise_5/app/src/main/java/com/example/exercise_5/MainActivity.java
+++ Exercise_5/app/src/main/java/com/example/exercise_5/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.exercise_5;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,6 +13,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        getSupportActionBar().setTitle("NEW TITLE HERRE");
+    }
+    void XuLy_BMI(View view){
+
+        EditText dk_H = (EditText) findViewById(R.id.edtH);
+        float SoA = Float.parseFloat(dk_H.getText().toString());
+        EditText dk_W = (EditText) findViewById(R.id.edtW);
+        float SoB = Float.parseFloat(dk_W.getText().toString());
+        float KetQua =SoA / SoB;
+        TextView dk_KQ = (TextView) findViewById(R.id.tvResult);
+        dk_KQ.setText( String.valueOf(KetQua) );
     }
 }
